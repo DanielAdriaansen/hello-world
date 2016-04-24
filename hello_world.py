bf68bee82b6b3b93caa3c9f0a16fec77be477826
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2016-04-24 11:49:27 -0600

    First commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# File: hello_world.py
+#
+# Author: D. Adriaansen
+#
+# Date: 24 Apr 2016
+#
+# Purpose: Testing out git repository behavior
+#
+# Notes:
+#_____________________________________________________
+
+import os, sys, glob, operator
+
+print("HELLO WORLD")
+
